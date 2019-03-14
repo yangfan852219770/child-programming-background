32ff77b443ae2dc585304fcec363b8daf4f33cb5
yangfan <852219770@qq.com>
2019-03-14 20:55:03 +0800

    跨域配置

---
+++ src/main/java/com/child/programming/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.child.programming.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Description：跨域请求配置
+ * @Author：yangfan
+ **/
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //TODO 开发环境  添加映射路径，目前是所有
+        registry.addMapping("/**")
+                //请求类型
+                .allowedMethods("GET", "POST")
+                //允许跨域网址
+                .allowedOrigins("http://localhost:8000");
+    }
+}
