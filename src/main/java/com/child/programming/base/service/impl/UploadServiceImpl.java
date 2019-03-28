fbd4fc76c9a2e25c71bb0bcc06994be4a505478c
yangfan <852219770@qq.com>
2019-03-28 17:29:20 +0800

    上传图片优化

--- src/main/java/com/child/programming/base/service/impl/UploadServiceImpl.java
+++ src/main/java/com/child/programming/base/service/impl/UploadServiceImpl.java
@@ -1,7 +1,9 @@
 package com.child.programming.base.service.impl;
 
 import com.child.programming.base.service.IUploadService;
+import com.child.programming.base.util.ConstDataUtil;
 import com.child.programming.base.util.DateUtil;
+import com.child.programming.base.util.EmptyUtils;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -30,10 +32,9 @@ public class UploadServiceImpl implements IUploadService {
     @Value("${IMAGE.ALLOWSIZE}")
     private String  allowSize;
 
-    /**
-     * 文件上传路径
-     */
-    private String serverUploadFilePath;
+    private Long getAlloSize(){
+       return Long.parseLong(allowSize) * 1024 * 1024;
+    }
 
 
     /**
@@ -47,13 +48,12 @@ public class UploadServiceImpl implements IUploadService {
     @Override
     public String[] uploadFiles(String businessType, MultipartFile[] files, HttpServletRequest request) {
         // 初始化设置文件上传路径
-        serverUploadFilePath = request.getSession().getServletContext().getRealPath("/");
          String[] filePaths=null;
         try {
             filePaths = new String[files.length];
             int index = 0;
             for (MultipartFile file : files) {
-                String suffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".") + 1);
+                /*String suffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".") + 1);
                 int length = allowSuffix.indexOf(suffix);
                 if (length == -1) {
                     log.error("请上传允许格式的文件！");
@@ -69,8 +69,11 @@ public class UploadServiceImpl implements IUploadService {
                 String fileNameNew = getFileNameNew() + "." + suffix;//
                 File f = new File(destFile.getAbsoluteFile() + "\\" + fileNameNew);
                 file.transferTo(f);
-                f.createNewFile();
-                filePaths[index++] = serverUploadFilePath + fileNameNew;
+                f.createNewFile();*/
+                String path = uploadFile(businessType, file, request);
+                if (EmptyUtils.stringIsEmpty(path))
+                    return null;
+                filePaths[index++] = path;
             }
         } catch (Exception e) {
             log.error(e.getMessage());
@@ -82,24 +85,26 @@ public class UploadServiceImpl implements IUploadService {
     public String uploadFile(String businessType, MultipartFile file, HttpServletRequest request) {
         System.out.println(businessType);
         // 初始化设置文件上传路径
-        serverUploadFilePath = request.getSession().getServletContext().getRealPath("/") + "upload_files/"
-                + businessType + "/";
+        String basePath = request.getSession().getServletContext().getRealPath(ConstDataUtil.FORWARD_SLASH)
+                + ConstDataUtil.UPLOAD_FILES + ConstDataUtil.FORWARD_SLASH;
 
-        String filePath = "";
+        String relativePath = businessType + ConstDataUtil.FORWARD_SLASH;
         try {
             String suffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".") + 1);
             int length = allowSuffix.indexOf(suffix);
             if (length == -1) {
                 log.error("请上传允许格式的文件！");
+                return null;
             }
-            if (file.getSize() > Long.parseLong(allowSize)) {
+            if (file.getSize() > getAlloSize()) {
                 log.error("上传的文件大小超出系统允许范围！");
+                return null;
             }
 
             // 格式化的年月日
             String nowDateTimeDir = DateUtil.DateToString(new Date(), "yyyy-MM-dd");
             // 根据年月日和taskId创建文件夹
-            String path = serverUploadFilePath + nowDateTimeDir + "/";
+            String path = basePath + relativePath + nowDateTimeDir + ConstDataUtil.FORWARD_SLASH;
 
             File destFile = new File(path);
             if (!destFile.exists()) {
@@ -107,15 +112,15 @@ public class UploadServiceImpl implements IUploadService {
             }
             String fileNameNewStr = getFileNameNew();
             String fileNameNew = fileNameNewStr + "." + suffix;
-            File f = new File(destFile.getAbsoluteFile() + "/" + fileNameNew);
+            File f = new File(destFile.getAbsoluteFile() + ConstDataUtil.FORWARD_SLASH + fileNameNew);
             file.transferTo(f);
             f.createNewFile();
 
-            filePath = path + fileNameNew;
+            relativePath += nowDateTimeDir + ConstDataUtil.FORWARD_SLASH + fileNameNew;
         } catch (Exception e) {
             log.error(e.getMessage());
         }
-        System.out.println(filePath);
-        return filePath;
+        log.info(relativePath);
+        return relativePath;
     }
 }
--- src/main/java/com/child/programming/base/util/ConstDataUtil.java
+++ src/main/java/com/child/programming/base/util/ConstDataUtil.java
@@ -6,7 +6,11 @@ package com.child.programming.base.util;
  **/
 public class ConstDataUtil {
 
-    public final static String CURRENT_USER = "currentUser";
+    public final static String CURRENT_USER = "currentUser"; // session中存放用户的key
 
-    public final static String DEFAULT_PASSWORD = "123456";
+    public final static String DEFAULT_PASSWORD = "123456"; // 默认密码
+
+    public final static String UPLOAD_FILES = "upload_files"; // TODO 临时上传目录
+
+    public final static String FORWARD_SLASH = "/"; // 正斜杠
 }
